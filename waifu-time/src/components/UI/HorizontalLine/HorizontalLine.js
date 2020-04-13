0f0347a831c6981a0e1d3dd372c32afa9a2aba43
Razvan Pirvu <vanos942@gmail.com>
2020-04-13 18:42:21 -0400

    Bug fixes

--- waifu-time/src/components/UI/HorizontalLine/HorizontalLine.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from './HorizontalLine.module.css';
-
-const HorizontalLine = () => {
-  return <div className={classes.line}></div>;
-};
-
-export default HorizontalLine;
